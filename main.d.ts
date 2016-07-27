31fb8d01a5dcad8a2ae8acd87d8db25c6765c16a
barbatus <alex.borodach@gmail.com>
2016-07-27 18:59:56 +0300

    Initial commit

---
+++ main.d.ts
@@ -0,0 +1,19 @@
+declare module "*.html" {
+  const template: string;
+  export default template;
+}
+
+declare module "*.less" {
+  const template: string;
+  export default template;
+}
+
+declare module "*.sass" {
+  const template: string;
+  export default template;
+}
+
+declare module "*.scss" {
+  const template: string;
+  export default template;
+}
